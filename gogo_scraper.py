283f166179110b7f2a560d11f7572e78aab4f162
alanjoshua <41203962+alanjoshua@users.noreply.github.com>
2020-12-04 17:26:31 +0530

    Updated getLatestEpisode to directly make ajax request instead of using selenium, and refactored validify method

--- gogo_scraper.py
+++ gogo_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import re
 
 
 options = webdriver.ChromeOptions()
@@ -11,19 +12,20 @@ options.add_argument("--headless")
 BASE_URL = 'https://gogoanime.so/'  # Change this if this link to gogoanime goes down
 
 
-def __validifyName__(name):
+def _validifyName_(name):
     """
 
     Inner method used to clean anime search query
 
     :param name: Anime search query
-    :return: Removes white space and adds '-' to better comply with Gogoanime URL specification
+    :return: Replaces special characters with '-' to comply with Gogoanime's URL specifications
     """
-    if len(name.split()) > 0:
-        name = str(name).lower()
-        newName = str(name).replace(' ', '-')
-        newName = newName.replace(":", '-')
-        newName = newName.replace("--", '-')
+
+    # Replace all special characters and get a normalized name where only - is between words, and words in in lowercase
+    newName = re.sub(r' +|:+|#+|%+|\?+|\^+|\$+|\(+|\)+|_+|&+|\*+ |\[+ |]+|\\+|{+|}+|\|+|/+|<+|>+|\.+|\'+', "-",
+                     name.lower())
+    newName = re.sub(r'-+', "-", newName)
+
     return newName
 
 
@@ -33,31 +35,31 @@ def getLatestEpisode(anime, base_url=BASE_URL):
 
     Gets the latest episode of the input anime
 
-    This function uses selenium with Chrome in headless mode, so make sure to have chromedriver be installed and added
-    to system Path, and have Chrome installed
-
     :param anime: Exact Gogoanime title/link
     :param base_url: Base Gogoanime URL. Useful if the current default URL gets taken down
     :return: Link and episode number of latest episode, or None if anime or episode was not found
     """
-    anime = __validifyName__(anime)
-    print(anime)
+    anime = _validifyName_(anime)
     newUrl = base_url + "category/" + anime
 
-    driver = webdriver.Chrome(options=options)
-    driver.get(newUrl)
+    page_response = requests.get(newUrl)
+    page = BeautifulSoup(page_response.content, "html.parser")
 
-    try:
-        episodes_list = driver.find_element_by_css_selector("ul[id='episode_related']")
-        lastEpisode = episodes_list.find_element_by_css_selector("a")
-        lastEpisode_num = int(lastEpisode.find_element_by_class_name("name").text.split()[1])
-        lastEpisode_num_part_link = lastEpisode.get_attribute("href")
-        lastEpisode_link = lastEpisode_num_part_link
-    except:
-        driver.close()
-        return None
+    # Extract variables from website to create ajax request
+    startEp = page.find('ul', id='episode_page').find('a', class_='active').get('ep_start')
+    endEp = page.find('ul', id='episode_page').find('a', class_='active').get('ep_end')
+    id = page.find('input', id='movie_id').get('value')
+    default_ep = page.find('input', id='default_ep').get('value')
+    alias = page.find('input', id='alias_anime').get('value')
 
-    driver.quit()
+    # Create ajax url and load data
+    loadEpisode = f'https://ajax.gogocdn.net/ajax/load-list-episode?ep_start={startEp}&ep_end={endEp}&id={id}&default_ep={default_ep}&alias={alias}'
+    episodesData = BeautifulSoup(requests.get(loadEpisode).content, "html.parser")
+
+    # Get last episode num from episodeData, and create episode link
+    lastEpisode_num = int(episodesData.find('div', class_='name').text.split()[1])
+    lastEpisode_link = base_url[:-1] + str(episodesData.find('a').get('href')).strip()
+
     return {"num": lastEpisode_num, "link": lastEpisode_link}
 
 
@@ -70,14 +72,13 @@ def getEpisode(anime, re_ep, base_url=BASE_URL):
     :param base_url: Base Gogoanime URL. Useful if the current default URL gets taken down
     :return: Link to requested episode, or None if episode or anime is not found
     """
-    anime = __validifyName__(anime)
+    anime = _validifyName_(anime)
     episodeUrl = base_url + anime + '-episode-' + str(re_ep)
     page_response = requests.get(episodeUrl)
     master_page = BeautifulSoup(page_response.content, "html.parser")
 
     try:
         master_page.find('h1', class_='entry-title').text
-        print('episode not found')
         return None
     except:
         return episodeUrl
@@ -93,7 +94,7 @@ def search(anime, base_url=BASE_URL):
     :return: List of search results
     """
 
-    anime = __validifyName__(anime)
+    anime = _validifyName_(anime)
     searchUrl = base_url + '/search.html?keyword='+anime
 
     page_response = requests.get(searchUrl)
@@ -107,6 +108,10 @@ def search(anime, base_url=BASE_URL):
         info['name'] = item.find('p', class_='name').find('a').text
         info['link'] = base_url + item.find('p', class_='name').find('a').get('href')
         info['released'] = item.find('p', class_='released').text.strip()
+        info['gogoTitle'] = info['link'][info['link'].find('category') + 9:]
         res.append(info)
 
     return res
+
+
+print(getLatestEpisode("naruto"))
