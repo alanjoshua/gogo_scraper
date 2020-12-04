1a5df1e4330d50b7a2f1cebf40de949ab63bad65
alanjoshua <41203962+alanjoshua@users.noreply.github.com>
2020-12-04 20:53:32 +0530

    added gogo_scraper to pypy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gogo_scraper',
+    version='1.0.4',
+    packages=[''],
+    url='https://github.com/alanjoshua/gogo_scraper',
+    license='MIT',
+    author='Alan Aneeth Jegaraj',
+    author_email='alanjoshua2002@gmail.com',
+    description='A simple web scraper to get information about anime from Gogoanime',
+)
